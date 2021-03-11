fff1c5893bd337906e003da37515d5814e518faa
ThatOneGuyRick <rickberg98@live.nl>
2021-03-11 15:14:47 +0100

    added cors

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors')
 require('dotenv').config()
 
 var indexRouter = require('./routes/index');
@@ -19,6 +20,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
 
 require("./models/index");
 
