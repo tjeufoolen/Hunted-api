7a116be41df292d7fb5d690d5cc888bb7e730275
ThatOneGuyRick <43992586+ThatOneGuyRick@users.noreply.github.com>
2021-03-11 15:38:27 +0100

    Feat/api setup (#1)
    * inbetween
    
    * inbetween
    
    * initial
    
    * changes to db connection
    
    * docker connection
    
    * migration and seeders
    
    * cleanup
    
    * inbetween
    
    * inbetween
    
    * merge
    
    * added cors
    
    * feedback
    
    Co-authored-by: Tjeu Foolen <12643433+tjeufoolen@users.noreply.github.com>

---
+++ README.md
@@ -0,0 +1,15 @@
+# Hunted-API
+REST api build for the Hunted project using express.
+
+## Getting Started
+1. Clone this repository.
+2. Run `docker-compose up -d --build`
+3. Start programming! :tada:
+
+## Authors
+- [Micha Nijenhof](https://github.com/nijenhof)
+- [Tjeu Foolen](https://github.com/tjeufoolen)
+- [Wouter van Mierlo](https://github.com/wvm28)
+- [Tim van den Berg](https://github.com/timvandenber9)
+- [Rick van den Berg](https://github.com/thatoneguyrick)
+- [Bart Fransen](https://github.com/Bartf6)
--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -18,24 +19,26 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
+
+require("./models/index");
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
+app.use(function (err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.json({
+    status: err.status,
+    error: err
+  });
 });
 
 module.exports = app;
