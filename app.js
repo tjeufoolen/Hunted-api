ec99d0741333bc399eb778ec228b945c4cf69862
ThatOneGuyRick <rickberg98@live.nl>
2021-03-11 15:37:22 +0100

    feedback

--- app.js
+++ app.js
@@ -3,8 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var cors = require('cors')
-require('dotenv').config()
+var cors = require('cors');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -34,10 +33,6 @@ app.use(function (req, res, next) {
 
 // error handler
 app.use(function (err, req, res, next) {
-  // set locals, only providing error in development 
-  // res.locals.message = err.message; 
-  // res.locals.error = req.app.get('env') === 'development' ? err : {};
-
   // render the error page
   res.status(err.status || 500);
   res.json({
