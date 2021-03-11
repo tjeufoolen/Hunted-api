1737d9a1d90c0c8c83b6a87a00e3e9a8ec0b7639
Tjeu Foolen <12643433+tjeufoolen@users.noreply.github.com>
2021-03-11 14:27:19 +0100

    inbetween

--- models/index.js
+++ models/index.js
@@ -5,10 +5,14 @@ const path = require('path');
 const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
+const config = require(__dirname + '/../config/config.js')[env];
 const db = {};
 
-let sequelize = new Sequelize(process.env.CLEARDB_DATABASE_URL || "mysql://developer:developer@mysqldb/hunted_api_db?reconnect=true")
+if (config.use_env_variable) {
+  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+} else {
+  sequelize = new Sequelize(config.database, config.username, config.password, config);
+}
 
 fs
   .readdirSync(__dirname)
