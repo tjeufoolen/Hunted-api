d5b8fe44beb89202e19b12f0227eb6174d770175
ThatOneGuyRick <rickberg98@live.nl>
2021-03-11 15:01:37 +0100

    merge

---
+++ config/config.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+
+module.exports = {
+  development: {
+    username: 'developer',
+    password: 'developer',
+    database: 'hunted_api_db',
+    host: 'mysqldb',
+    port: 3306,
+    dialect: 'mysql'
+  },
+  test: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: 3306,
+    dialect: 'mysql'
+  }
+};
