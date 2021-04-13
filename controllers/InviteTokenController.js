1e02ab4c363457ee381231228768c343aed26a7a
Tjeu Foolen <12643433+tjeufoolen@users.noreply.github.com>
2021-04-13 13:44:58 +0200

    code cleanup and comment blocks

--- controllers/InviteTokenController.js
+++ controllers/InviteTokenController.js
@@ -2,6 +2,13 @@ const { Controller } = require('./Controller');
 const BCrypt = require('bcryptjs');
 
 class InviteTokenController extends Controller {
+    /**
+     * Generates a single token based on the specified gameId and playerId.
+     * 
+     * @param {*} gameId 
+     * @param {*} playerId 
+     * @returns generated token
+     */
     generate(gameId, playerId) {
         const unhashedToken = `${gameId}-${playerId}-${process.env.INVITE_CODE_KEY}`;
 
@@ -16,9 +23,18 @@ class InviteTokenController extends Controller {
         return token;
     }
 
-    generateMultiple(amount, gameId, playerId) {
+    /**
+     * Generates multiple invite tokens based on the specified gameId
+     * 
+     * @param {*} amount of tokens to generate
+     * @param {*} gameId 
+     * @param {*} playerId 
+     * @returns array containing the generated tokens
+     */
+    generateMultiple(amount, gameId) {
         const tokens = [];
         for (let i = 0; i < amount; i++) {
+            const playerId = i + 1; // Start counting from 1 instead of 0
             tokens.push(this.generate(gameId, playerId));
         }
         return tokens;
