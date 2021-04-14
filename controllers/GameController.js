078e679fb65911d31d675efbe4b3078f2f893311
Tjeu Foolen <12643433+tjeufoolen@users.noreply.github.com>
2021-04-14 14:55:55 +0200

    Fixed join endpoint

--- controllers/GameController.js
+++ controllers/GameController.js
@@ -22,29 +22,16 @@ class GameController extends Controller {
         // Check if invite token is present
         if (!req.params.code) return this.error(next, 400, "Missing invite token");
 
-        // //TODO: Add if Bart & Tim's code has been merged and the player and game classes are available.
-        // // Check if token is from a player
-        // const player = await Player.findOne({
-        //     where: {
-        //         code: req.params.code
-        //     }
-        // });
-        // if (!player) return this.error(next, 400, "Invalid invite token");
+        // Check if token is from a player
+        const player = await Player.findOne({
+            where: {
+                code: req.params.code
+            }
+        });
+        if (!player) return this.error(next, 400, "Invalid invite token");
 
-        // // Return player 
-        // ResponseBuilder.build(res, 200, player);
-
-        // temp
-        ResponseBuilder.build(res, 200, {
-            id: 6969696969,
-            game: {
-                id: 3420234
-            },
-            code: '67477-15e87-46818-a097e-aba3c-2aab4-02c53-1f2aa',
-            playerRole: 0,
-            outOfTheGame: false,
-            location: null
-        }); // END temp
+        // Return player 
+        ResponseBuilder.build(res, 200, player);
     }
 
     async create(req, res, next) {
