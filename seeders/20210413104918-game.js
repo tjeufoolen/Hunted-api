72f69523a5b819df98f37c4b04dedc99f3e1f1b5
Tjeu Foolen <12643433+tjeufoolen@users.noreply.github.com>
2021-04-15 16:59:40 +0200

    momentjs

--- seeders/20210413104918-game.js
+++ seeders/20210413104918-game.js
@@ -1,15 +1,17 @@
 'use strict';
 
+const moment = require('moment');
+
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     return queryInterface.bulkInsert('Games', [{
       userId: 1,
-      startAt: "2021-05-13",
+      startAt: moment.utc().format('YYYY-MM-DD HH:mm:ss'),
       minutes: 60,
       layoutTemplateId: 0,
-    },{
+    }, {
       userId: 1,
-      startAt: "2021-06-13",
+      startAt: moment.utc().format('YYYY-MM-DD HH:mm:ss'),
       minutes: 90,
       layoutTemplateId: 0,
     }],
