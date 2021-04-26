fa09ae4536b713fdd424458cf05353013611774b
ThatOneGuyRick <rickberg98@live.nl>
2021-04-26 11:14:56 +0200

    todo message

--- app.js
+++ app.js
@@ -25,6 +25,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(cors());
 
+// ToDo: for testing
 app.get("/", (req, res, next) => {
 	res.sendFile(__dirname + "/test.html")
 })
