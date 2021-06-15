c8a26388d64054dea18b7425d6da085664b710d5
timvandenber9 <48826364+timvandenber9@users.noreply.github.com>
2021-06-15 22:47:07 +0200

    Feature/choose winner (#36)
    * initial setup
    
    first try solution
    
    * update Gamecontroller
    
    playerroles toevoegen
    
    * [UPDATE] Game patch
    
    Co-authored-by: Bart Fransen <bartfransen66@gmail.com>
    Co-authored-by: Tim van den Berg <timvandenberg@livewallgroup.com>

--- controllers/GameController.js
+++ controllers/GameController.js
@@ -8,6 +8,7 @@ const InviteTokenController = require('./InviteTokenController');
 const CronManager = require('../managers/CronManager');
 const io = require('../utils/socket');
 const geolib = require('geolib');
+const { PlayerRoles } = require("../enums/PlayerRoles");
 
 class GameController extends Controller {
     constructor() {
@@ -19,6 +20,7 @@ class GameController extends Controller {
         this.getById = this.getById.bind(this);
         this.update = this.update.bind(this);
         this.delete = this.delete.bind(this);
+        this.patch = this.patch.bind(this);
         this.startCronjob = this.startCronjob.bind(this);
     }
 
@@ -192,6 +194,50 @@ class GameController extends Controller {
         game.gameAreaRadius = req.body.gameAreaRadius;
         game.isStarted = req.body.isStarted;
         game.interval = req.body.interval;
+        if (req.body.winner != undefined && req.body.winner != null) game.winner = req.body.winner;
+
+        // Save updated game
+        const updatedGame = await game.save();
+
+        // Notify socket game is starting if isStarted changed this request
+        if (oldGameStarted != updatedGame.isStarted) {
+            io.to(updatedGame.id).emit("gameStarted");
+        }
+
+        // Start cronjob if it wasn't already running when the game started
+        if (updatedGame.isStarted && !CronManager.running(updatedGame.id)) {
+            await this.startCronjob(updatedGame, next);
+        }
+
+        // Return updated game
+        ResponseBuilder.build(res, 200, updatedGame);
+    }
+
+    async patch(req, res, next) {
+        // Handle top level route /user/:userId
+        if (req.params.userId) {
+            // Check if authenticated user has permission to create game under specified userId
+            if (!req.user.isAdmin && (req.user.id != req.params.userId)) {
+                return this.error(next, 403, 'Unauthorized');
+            }
+        }
+
+        // validate data
+        const error = this.validatePatch(req.body);
+        if (error) return this.error(next, 400, 'Incomplete data');
+
+        // Create game
+        const game = await Game.findOne({
+            where: {
+                id: req.params.gameId
+            }
+        });
+        if (!game) return this.error(next, 404, 'The specified game could not be found', 'game_not_found')
+
+        // keep copy of old data to compare against
+        const oldGameStarted = game.isStarted;
+
+        if (req.body.winner != undefined && req.body.winner != null) game.winner = req.body.winner;
 
         // Save updated game
         const updatedGame = await game.save();
@@ -317,9 +363,16 @@ class GameController extends Controller {
             gameAreaRadius: Joi.number().min(0).required(),
             gameAreaLatitude: Joi.number().required(),
             gameAreaLongitude: Joi.number().required(),
-            interval: Joi.number().min(1).max(15).required()
+            interval: Joi.number().min(1).max(15).required(),
         });
 
+        return schema.validate(data).error;
+    }
+
+    validatePatch(data) {
+        const schema = Joi.object({
+            winner: Joi.number().valid(...PlayerRoles.values())
+        });
         return schema.validate(data).error;
     }
 
